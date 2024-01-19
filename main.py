56d387754a2455cbe78cd95120b085e407ad0cd3
Expliyh <expliyh@outlook.com>
2024-01-19 17:06:11 +0800

    初始提交

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from telegram import Update
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+async def bot():
+    pass
