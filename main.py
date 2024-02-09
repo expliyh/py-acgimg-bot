8655279970829ebe5714c9ed660f0b707698884b
Expliyh <expliyh@outlook.com>
2024-02-09 12:28:31 +0800

    添加 TgBot Webhook

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
-from telegram import Update
+from telegram import Update, Bot
+from telegram.ext import Application, CallbackContext, CommandHandler
 
 app = FastAPI()
+
+
+async def start(update: Update, context) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
+
+
+TELEGRAM_TOKEN = 'AAAA'
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+tg_app = Application.builder().token(TELEGRAM_TOKEN).build()
+
+tg_app.add_handler(CommandHandler("start", start))
 
 
 @app.get("/")
@@ -15,5 +29,11 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-async def bot():
-    pass
+@app.post("/tapi/")
+async def telegram_webhook(request: Request):
+    update = Update.de_json(await request.json(), bot)
+
+    await tg_app.update_queue.put(update)
+
+    return {'ok': True}
+
