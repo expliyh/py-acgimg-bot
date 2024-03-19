7ef2f290bb05016f96711ebfd25dc47d1c215705
Expliyh <expliyh@outlook.com>
2024-03-19 20:00:16 +0800

    Update

---
+++ bot.py
@@ -0,0 +1,41 @@
+import logging
+
+from fastapi import Request
+
+from telegram import Update, Bot
+from telegram.ext import Application, CallbackContext, CommandHandler
+
+from configs import config
+from registries import config_registry
+
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context) -> None:
+    logger.info(f"Update START for user: {update.effective_user}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
+
+
+class TelegramBot:
+    def __init__(self):
+        self.tg_bot: Bot | None = None
+        self.tg_app: Application | None = None
+
+    async def config(self):
+        tokens = await config_registry.get_bot_tokens()
+        if len(tokens) > 1:
+            raise Exception("Too many bot tokens, Multiple bot tokens are currently not supported")
+        if len(tokens) == 0:
+            raise Exception("No bot token found")
+        token = tokens[0]
+        self.tg_bot = Bot(token.token)
+        await self.tg_bot.set_webhook(f"https://{config.external_url}/tapi/")
+        self.tg_app = Application.builder().token(token.token).build()
+        self.tg_app.add_handler(CommandHandler("start", start))
+
+    async def put_update(self, request: Request) -> None:
+        update = Update.de_json(await request.json(), self.tg_bot)
+        self.tg_app.update_queue.put(update)
+
+
+tg_bot = TelegramBot()
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import logging
 from fastapi import FastAPI, Request
 
 from telegram import Update, Bot
-from telegram.ext import Application, CallbackContext, CommandHandler
+
+import bot
+from bot import tg_bot
 
 from contextlib import asynccontextmanager
 from registries import engine, config_registry
+import uvicorn
 
 from configs import config
 
@@ -21,28 +24,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-async def start(update: Update, context) -> None:
-    logger.info(f"Update START for user: {update.effective_user}")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
-
-
 # TELEGRAM_TOKEN = 'AAAA'
-
-pool = asyncio.get_event_loop()
-
-pool.run_until_complete(engine.create_all())
-tokens = pool.run_until_complete(config_registry.get_bot_tokens())
-if len(tokens) > 1:
-    raise Exception("Too many bot tokens, Multiple bot tokens are currently not supported")
-if len(tokens) == 0:
-    raise Exception("No bot token found")
-token = tokens[0]
-bot = Bot(token.token)
-pool.run_until_complete(bot.set_webhook(f"https://{config.external_url}/tapi/"))
-tg_app = Application.builder().token(token.token).build()
-tg_app.add_handler(CommandHandler("start", start))
-tg_ready = True
-logger.warning("Bot started")
 
 
 # tg_app.add_handler(CommandHandler("start", start))
@@ -50,6 +32,9 @@ logger.warning("Bot started")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await engine.create_all()
+    await tg_bot.config()
+    logger.warning("Bot started")
     yield
     pass
 
@@ -59,10 +44,6 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
-    if not tg_ready:
-        logger.warning("BOT NOT READY")
-        logger.warning(tg_app)
-        return
     return {"message": "Hello World"}
 
 
@@ -73,13 +54,11 @@ async def say_hello(name: str):
 
 @app.post("/tapi/")
 async def telegram_webhook(request: Request):
-    if not tg_ready:
-        logger.warning("BOT NOT READY")
-        logger.warning(tg_app)
-        return
 
-    update = Update.de_json(await request.json(), bot)
-
-    await tg_app.update_queue.put(update)
+    await tg_bot.put_update(request)
 
     return {'ok': True}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
