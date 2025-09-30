d940f24f5b7a148b87e70ea153ff4b6035b37170
Yuheng Li <liyuheng@xiaomi.com>
2025-09-30 23:45:24 +0800

    feat: persist chat history for group and private messages

--- handlers/all_handlers.py
+++ handlers/all_handlers.py
@@ -1,8 +1,10 @@
-from telegram.ext import BaseHandler, CallbackQueryHandler
+from telegram.ext import BaseHandler, CallbackQueryHandler, MessageHandler, filters
 
 from .callback_handlers import callback_handler_func
 from .command_handlers import all_command_handlers
+from .message_handlers.root import handle_incoming_message
 
 all_handlers: [BaseHandler] = []
+all_handlers.append(MessageHandler(filters.ALL, handle_incoming_message, block=False))
 all_handlers.extend(all_command_handlers)
 all_handlers.append(CallbackQueryHandler(callback_handler_func))
